a1fe9b03f42c38f925708da0ffe2569e5f748a4c
Ryan <84257290+ryantng05@users.noreply.github.com>
2025-06-14 23:54:41 -0400

    Create home.jsx

---
+++ home.jsx
@@ -0,0 +1,28 @@
+
+
+export default function Home() {
+
+    return(
+        <>
+        <div className="ml-50 ">
+            <div className="mt-82 mb-10">
+
+            <h1>Hi</h1>
+            <br/>
+            <h1>I'm Ryan,</h1>
+            <br/>
+            <h1>aspiring software engineer.</h1>
+            <br/>
+            <h2>Computer Science & AI, 2027 at Wilfrid Laurier Univeristy</h2>
+
+        </div>
+
+        <button className="border-1 p-1 cursor-pointer text-rose-500 hover:bg-rose-500 hover:text-black hover:border-rose-500">
+            CONTACT ME
+        </button>
+        </div>
+        </>
+
+    );
+
+}
