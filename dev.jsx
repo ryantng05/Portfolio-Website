f6e4cd9de60129cdda0847a02e52633beaa9a891
Ryan <84257290+ryantng05@users.noreply.github.com>
2025-06-17 00:22:33 -0400

    Add files via upload

---
+++ dev.jsx
@@ -0,0 +1,10 @@
+
+export default function Dev() {
+    return(
+        <>
+            <div className="ml-50">
+                    <h1>hey</h1>
+            </div>
+        </>
+    )
+}
---
+++ tech.jsx
@@ -0,0 +1,10 @@
+
+export default function Tech() {
+    return(
+        <>
+            <div className="ml-50">
+                <h1>hey</h1>
+            </div>
+        </>
+    )
+}
