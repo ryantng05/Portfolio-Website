85f7590b72ab637cdcb7e67301366ea8e63a4091
Ryan <ryant012015@gmail.com>
2025-07-02 16:14:05 -0400

    commit

--- personal-website/src/pages/contact.jsx
+++ personal-website/src/pages/contact.jsx
@@ -1,3 +1,5 @@
+import { AiFillLinkedin, AiOutlineMail } from 'react-icons/ai';
+
 export default function Contact() {
 
     return(
@@ -6,8 +8,18 @@ export default function Contact() {
                 <h1 className="text-5xl font-bold text-yellow-400 mb-8 mt-20">Contact</h1>
                 <div className="ml-5 text-1xl">
                     <p>
-                    Thank you for your interest in getting in touch <br></br> I welcome any inquires, feedback, or collaboration opportunities
+                    Thank you for your interest in getting in touch! <br></br><br></br>I value open communication and welcome any inquiries,<br></br>
+                     feedback, or collaboration opportunities. Please don't hesitate<br></br> to get in touch with me by filling out the contact form.
                     </p>
+                </div>
+
+                <div className='mt-25 ml-5'>
+                    <button className='mr-40 cursor-pointer'>
+                    <AiFillLinkedin></AiFillLinkedin>
+                    </button>
+                    <button className='cursor-pointer'>
+                        <AiOutlineMail></AiOutlineMail>
+                    </button>
                 </div>
             </div>
         </>
