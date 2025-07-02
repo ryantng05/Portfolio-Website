41ef00cbe1c85175abceed084095d2ec7f76600a
Ryan <ryant012015@gmail.com>
2025-07-02 15:56:39 -0400

    commit

--- personal-website/src/pages/contact.jsx
+++ personal-website/src/pages/contact.jsx
@@ -3,7 +3,12 @@ export default function Contact() {
     return(
         <>
             <div className="ml-50">
-                <h1>hey there</h1>
+                <h1 className="text-5xl font-bold text-yellow-400 mb-8 mt-20">Contact</h1>
+                <div className="ml-5 text-1xl">
+                    <p>
+                    Thank you for your interest in getting in touch <br></br> I welcome any inquires, feedback, or collaboration opportunities
+                    </p>
+                </div>
             </div>
         </>
     )
